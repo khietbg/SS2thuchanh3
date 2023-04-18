5e39241a1796e200ff1679f7ce59a29d67d39b29
minhkhiet14151993 <119550390+minhkhiet14151993@users.noreply.github.com>
2023-04-18 15:20:46 +0700

    Add files via upload
    thuchanh3

---
+++ SS2thuchanh3.sql
@@ -0,0 +1,20 @@
+use thuchanh3;
+CREATE TABLE Mark
+(
+    MarkId    INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    SubId     INT NOT NULL,
+    StudentId INT NOT NULL,
+    Mark      FLOAT   DEFAULT 0 CHECK ( Mark BETWEEN 0 AND 100),
+    ExamTimes TINYINT DEFAULT 1,
+    UNIQUE (SubId, StudentId),
+    FOREIGN KEY (SubId) REFERENCES Subject (SubId),
+    FOREIGN KEY (StudentId) REFERENCES Student (StudentId)
+);
+
+
+
+
+
+
+
+
